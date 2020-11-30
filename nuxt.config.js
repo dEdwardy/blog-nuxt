890bb2d3568e3e1d8b0127ce2b93ce40aeffece7
edw4rd <957196958@qq.com>
2020-11-30 11:00:20 +0800

    add svg-sprite-loader

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,4 @@
+import { resolve } from 'path'
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
@@ -19,7 +20,8 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    '@/plugins/element-ui'
+    '@/plugins/element-ui',
+    '@/plugins/svg-icon' //注册 svg-icons插件文件 
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
@@ -36,9 +38,9 @@ export default {
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
+    // '@nuxtjs/pwa',
     // https://go.nuxtjs.dev/content
-    '@nuxt/content',
+    // '@nuxt/content',
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
@@ -50,5 +52,18 @@ export default {
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
     transpile: [/^element-ui/],
+    extend(config, context){
+      // 排除 nuxt 原配置的影响,Nuxt 默认有vue-loader,会处理svg,img等
+      // 找到匹配.svg的规则,然后将存放svg文件的目录排除
+      const svgRule =   config.module.rules.find( rule => rule.test.test('.svg'));
+      svgRule.exclude = [ resolve(__dirname,'assets/icons/svg')];
+
+      config.module.rules.push({
+        test:/\.svg$/,
+        //将存放svg的目录加入到loader处理目录
+        include:[resolve(__dirname,'assets/icons/svg')],
+        use: [{ loader: 'svg-sprite-loader',options: {symbolId: 'icon-[name]'}}]
+      })
+    }
   }
 }
